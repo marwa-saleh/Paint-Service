04b0a701620fe5389f14f7ac69b0e07d2c8c5680
Marwa Saleh <marwasaleh@microsoft.com>
2022-09-20 19:37:20 -0700

    setup-project

---
+++ Paint-Service/Paint-Service/Controllers/PaintController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NETCORE_TS_MVC.Controllers
+{
+    public class PaintController : Controller
+    {
+        private readonly ILogger<PaintController> _logger;
+
+        public PaintController(ILogger<PaintController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Paint()
+        {
+
+            //create session Id
+            return RedirectToAction("getSession", new { sessionId = 123 });
+        }
+
+        public IActionResult getSession(long sessionId)
+        {
+
+            return View();
+        }
+
+
+        public ActionResult GetClientMessages()
+        {
+
+            return Content("Success");
+        }
+
+
+
+    }
+}
---
+++ Paint-Service/Paint-Service/scripts/app.ts
@@ -0,0 +1,28 @@
+﻿function TSButton() {
+    //let name: string = "Fred";
+    //document.getElementById("ts-example").innerHTML = greeter(user);
+    let name: string = "Fred";
+    const input = document.getElementById("ts-example") as HTMLInputElement;
+    $.ajax({
+        method: "GET",
+        url: "GetClientMessages",
+    }).done((data) => input.innerHTML += '\n' + data);
+}
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Fred", "M.", "Smith");
